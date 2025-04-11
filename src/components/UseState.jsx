e6dbb4320eb58d03ec8b04df181f6eccd023d93e
yuliana <yuli.mejiap@gmail.com>
2025-04-10 20:37:19 -0500

    se crea codio para useEffect

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,12 +1,16 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 export default function UseState() {
     let[contador, setContador] = useState(0);// valor inicial en el que arranca el contador 
     let[mostrar,setMostrar]= useState(true);
     //let contador = 0;
+    useEffect(()=>{
+        console.log(contador);
+        document.title = `Contador ${contador}`;
+    },[contador]);
   return (
     <>
         {mostrar && <div>
-            <p>Contar{contador}</p>
+            <p>Contar {contador}</p>
             <button onClick ={()=> setContador(contador+1)}
             >Click aqui</button>
         </div>}
