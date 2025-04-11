c9156800c4f8fba81cec78e08e593b4b31776470
yuliana <yuli.mejiap@gmail.com>
2025-04-10 19:51:52 -0500

    se modifica el codigo UseState.jsx

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 export default function UseState() {
-    let[contador, setContador ] = (UseState(0));
+    let[contador, setContador] = useState(0);// valor inicial en el que arranca el contador 
+    let[mostrar,setMostrar]= useState(true);
     //let contador = 0;
   return (
     <>
-        <p>Contar{contador}</p>
-        <button onClick={()=> setContador(contador++)}
-        >Click aqui</button>
+        {mostrar && <div>
+            <p>Contar{contador}</p>
+            <button onClick ={()=> setContador(contador+1)}
+            >Click aqui</button>
+        </div>}
+        <button onClick={()=> setMostrar(!mostrar)}
+        >Mostrar/Ocultar</button>      
     </>
   )
 }
