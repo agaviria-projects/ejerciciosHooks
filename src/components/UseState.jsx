ee768bcc7a84b227091cb34a9717605cd0da07c7
yuliana <yuli.mejiap@gmail.com>
2025-04-10 19:35:49 -0500

    se crea codigo para el componente UseState.jsx

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import { useState } from "react";
 export default function UseState() {
+    let[contador, setContador ] = (UseState(0));
+    //let contador = 0;
   return (
-    <div>
-        Use estate
-    </div>
+    <>
+        <p>Contar{contador}</p>
+        <button onClick={()=> setContador(contador++)}
+        >Click aqui</button>
+    </>
   )
 }
